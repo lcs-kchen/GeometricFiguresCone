49f4e2a0ab94bece40a7ab346b25a64066cfae50
Kevin Chen <kchen@lcs.on.ca>
2024-10-29 12:22:29 -0400

    Added in texts that creates output of the information of the cone, and slider that describe the basic component of the cone

--- GeometricFiguresCone/Model/View/ConeView.swift
+++ GeometricFiguresCone/Model/View/ConeView.swift
@@ -13,10 +13,58 @@ struct ConeView: View {
     var body: some View {
         VStack {
             
+            Slider(
+                value: $currentCone.radius,
+                in: 1...100,
+                step: 1.0
+                
+            )
+            
+            Slider(
+                value: $currentCone.height,
+                   in: 1...100,
+                   step: 1.0
+                   )
+            
+            Slider (
+                value: $currentCone.sideLength,
+                in: 1...100,
+                step: 1.0
+                )
+            
+            
+            //Label (show the current slider value)
+            Text("Radius is: \(currentCone.radius.formatted())")
+            
+            
+            //Label (show the circumference)
+            Text("Height is: \(currentCone.height.formatted())")
+            
+            
+            //Label (show the area)
+            Text("Side Length is: \(currentCone.sideLength.formatted())")
+            
+            //Label (show the diameter)
+            Text("Diameter is: \(currentCone.diameter.formatted())")
+            
+            //Label (show the base area)
+            Text("Base Area is: \(currentCone.baseArea.formatted())")
+            
+            //Label (show the lateral surface)
+            Text("Lateral surface area is: \(currentCone.lateralSurface.formatted())")
+            
+            //Label (show the surface area)
+            Text("Surface Area is: \(currentCone.totalSurfaceArea.formatted())")
+            
+            //Label (show the volume)
+            Text("volume is: \(currentCone.volume.formatted())")
+            
+            Spacer()
+        }
         }
     }
     
-}
+
 
 #Preview {
     ConeView()
